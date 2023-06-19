07d52c9e25a20fec799497b7d254e6f0280adf16
yarov <yaroslav.vinokurov@gmail.com>
2023-06-19 18:55:21 -0400

    Added GrpcCurrencyServiceTest

--- niffler-currency/src/test/java/guru/qa/niffler/service/GrpcCurrencyServiceTest.java
+++ niffler-currency/src/test/java/guru/qa/niffler/service/GrpcCurrencyServiceTest.java
@@ -1,14 +1,21 @@
 package guru.qa.niffler.service;
 
+import com.google.protobuf.Empty;
+import guru.qa.grpc.niffler.grpc.CalculateRequest;
+import guru.qa.grpc.niffler.grpc.CalculateResponse;
+import guru.qa.grpc.niffler.grpc.CurrencyResponse;
 import guru.qa.niffler.data.CurrencyEntity;
 import guru.qa.niffler.data.repository.CurrencyRepository;
+import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
@@ -19,7 +26,8 @@ import static guru.qa.niffler.data.CurrencyValues.EUR;
 import static guru.qa.niffler.data.CurrencyValues.KZT;
 import static guru.qa.niffler.data.CurrencyValues.RUB;
 import static guru.qa.niffler.data.CurrencyValues.USD;
-import static org.mockito.Mockito.lenient;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GrpcCurrencyServiceTest {
@@ -72,5 +80,23 @@ class GrpcCurrencyServiceTest {
                 desiredCurrency, testCurrencies);
 
         Assertions.assertEquals(expectedResult, result.doubleValue());
+    }
+
+    @Test
+    void courseForCurrency() {
+        StreamObserver<CalculateResponse> responseObserverMock = mock(StreamObserver.class);
+        CalculateRequest calculateRequest = CalculateRequest.getDefaultInstance();
+        grpcCurrencyService.calculateRate(calculateRequest, responseObserverMock);
+        verify(responseObserverMock, times(1)).onNext(any());
+        verify(responseObserverMock, times(1)).onCompleted();
+    }
+
+    @Test
+    void calculateRate() {
+        StreamObserver<CurrencyResponse> responseObserver = mock(StreamObserver.class);
+        Empty request = Empty.getDefaultInstance();
+        grpcCurrencyService.getAllCurrencies(request, responseObserver);
+        verify(responseObserver, times(1)).onNext(any());
+        verify(responseObserver, times(1)).onCompleted();
     }
 }

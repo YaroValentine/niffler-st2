a19e4df5f9b4911b9b8ba6ab3c93f49665789223
yarov <yaroslav.vinokurov@gmail.com>
2023-06-30 15:38:12 -0400

    Updated NifflerUserdataGrpcTest.testGetAllUsers

--- niffler-e-2-e-homeworks/src/test/java/niffler/test/grpc/NifflerUserdataGrpcTest.java
+++ niffler-e-2-e-homeworks/src/test/java/niffler/test/grpc/NifflerUserdataGrpcTest.java
@@ -1,19 +1,25 @@
 package niffler.test.grpc;
 
 import com.google.protobuf.Descriptors;
+import guru.qa.grpc.niffler.grpc.User;
 import guru.qa.grpc.niffler.grpc.UserResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class NifflerUserdataGrpcTest extends BaseGrpcTest {
 
     @Test
     public void testGetAllUsers() {
         UserResponse allUsers = userdataStub.getAllUsers(EMPTY);
-        Map<Descriptors.FieldDescriptor, Object> allFields = allUsers.getAllFields();
-        Assertions.assertTrue(!allFields.isEmpty());
+
+        boolean userExists = allUsers.getUsersList().stream()
+                .anyMatch(u -> u.getUsername().equals("yaro"));
+
+        Assertions.assertTrue(userExists);
     }
 
 }
